a92e2a06f54970ac56fc73f1f5e9c3061c78a116
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-08-14 20:45:35 +0900

    Add Grand slam to map

--- app/javascript/home/tennis-map.js
+++ app/javascript/home/tennis-map.js
@@ -15,7 +15,29 @@ $(function () {
             latitude: 40.717079,
             longitude: -74.00116,
             tooltip: {content: "New York"},
-            href: "/"
+            href: "/",
+            size: 10,
+        }, 
+        'me': {
+            latitude: -37.814,
+            longitude: 144.96332,
+            tooltip: {content: "Melbourne"},
+            href: "/",
+            size: 10,
+        }, 
+        'pa': {
+            latitude: 48.85341,
+            longitude: 2.3488,
+            tooltip: {content: "Pari"},
+            href: "/",
+            size: 10,
+        },
+        'lo': {
+            latitude: 51.3026,
+            longitude: 0.0739,
+            tooltip: {content: "London"},
+            href: "/",
+            size: 10,
         }
         }
     });
