7031539facba988abfbd958dd0e2647fb8be3680
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-08-16 22:45:45 +0900

    Add ajax to rails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @msg = "worldd"
   end
+
+  def get_tournament_info
+    tournament_info = {id: 1}
+    render json: tournament_info
 end
