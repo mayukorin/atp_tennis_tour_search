5b7a99d71617bde06d8e7ee94ba544739bb5fbd4
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-08-17 18:06:45 +0900

    modify front to add tournament_info to map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,15 @@ class StaticPagesController < ApplicationController
   end
 
   def get_tournament_info
-    tournament_info = {id: 1}
+    tournament_info = { ME: {now_flag: 0, name: '全豪オープン', city: 'メルボルン', period: '2/11~2/22', champion: 'N ジョコビッチ'}, 
+                        NY: {now_flag: 1, name: '全米オープン', city: 'ニューヨーク', period: '8/11=8/22', champion: '',
+                            main_players: {
+                             2 => {name: 'ジョコビッチ', flag: 0},
+                             1 => {name: '錦織圭', flag: 1}
+                            }
+                            }
+                      }
+      
     render json: tournament_info
+  end
 end
