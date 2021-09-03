f621599ede6ada266bd96c86d49afe25ac08dc6f
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-03 23:53:07 +0900

    Add date of matach select box

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -2,6 +2,7 @@ class TournamentYearsController < ApplicationController
 
     def show
         @tournament_year = TournamentYear.pre_loading_of_players_and_matches.find(params[:id])
+        @dates = Match.select("date(day)").joins(:tournament_year).where(tournament_year: params[:id]).group("date(day)")
     end
     
 end
