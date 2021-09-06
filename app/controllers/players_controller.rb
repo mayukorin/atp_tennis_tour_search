4a73b1f9c511e4063fec7ad6db34e4bdb9a015f7
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-06 22:48:43 +0900

    Rename @player_matches to @selected_tournament_year_player_matches to align  name to @selected_tournament_day_matches

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,7 +7,7 @@ class PlayersController < ApplicationController
       [typ.tournament_year.tournament.name + "(" + typ.tournament_year.first_day.strftime("%Y") + ")", typ.tournament_year.id]
     end
     @selected_tournament_year_id = params[:tournament] ? params[:tournament]  : @tournament_years_array[0][1]
-    @player_matches = PlayerMatch.eager_loading.where(matches: {tournament_year: @selected_tournament_year_id}, player: @player.id)
+    @selected_tournament_year_player_matches = PlayerMatch.eager_loading.where(matches: {tournament_year: @selected_tournament_year_id}, player: @player.id)
     
   end
 end
