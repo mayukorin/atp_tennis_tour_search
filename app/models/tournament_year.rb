b488d19f58469f6ef0b9d32a148c5c3267cb1ead
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-06 22:52:03 +0900

    delete comment out

--- app/models/tournament_year.rb
+++ app/models/tournament_year.rb
@@ -6,5 +6,4 @@ class TournamentYear < ApplicationRecord
   has_many :matches, ->{order(day: :desc)}
   belongs_to :champion, class_name: 'Player', :foreign_key => 'champion_id', optional: true
   scope :eager_loading, -> { eager_load(:tournament, :champion, {tournament_year_and_players: :player}) }
-  scope :eager_loading_of_matches, -> { eager_load(tournament_year_and_players: :player, matches: [:home_player, :away_player, :win_player, :tournament_year]) }
 end
