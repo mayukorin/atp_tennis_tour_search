c49fc984260de7d75516ae40648c8a4e4777561c
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-12 01:03:30 +0900

    Add user model and user_new.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'players/new'
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get '/get_tournament_info', to: 'static_pages#get_tournament_info'
   resources :tournament_years, only: [:show]
   resources :players, only: [:show]
+  resources :users, only: [:new]
 end
