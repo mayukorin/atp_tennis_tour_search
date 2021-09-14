00101e92e099a357cbd00695b09740570e4f289e
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-14 16:03:47 +0900

    Create login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'players/new'
   root 'static_pages#home'
@@ -8,4 +9,7 @@ Rails.application.routes.draw do
   resources :tournament_years, only: [:show]
   resources :players, only: [:show]
   resources :users, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
