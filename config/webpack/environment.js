3e0cfecd9fb2823b5c436ead952d14f2282e9a53
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-20 00:11:15 +0900

    Modify jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,7 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
+    // $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery'
   })
 )
