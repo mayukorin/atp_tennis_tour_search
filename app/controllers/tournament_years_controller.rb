9e9e534acf1247d07367f9d7773cce141c300882
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-19 22:03:43 +0900

    Add ajax to day_select_form

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -1,20 +1,11 @@
 class TournamentYearsController < ApplicationController
 
     def show
-
-        @years_array = TournamentYear.joins(:tournament).where(tournament: 3).order(:first_day :desc).map do |tournament_year|
-            [tournament_year.first_day.strftime('%Y'), tournament_year.id]
-        end
-        @selected_tournament_year_id = params[:tournament_year_id] ? params[:tournament_year_id] : @years_array[0][1]
-        # @tournament_year = TournamentYear.eager_loading.find(params[:id])
-        @tournament_year = TournamentYear.eager_loading.find(@selected_tournament_year_id)
-        @tournament_days_array = Match.joins(:tournament_year).select("day::date").where(tournament_year: params[:id]).group("day::date").order("day::date desc").map do |tournament_day|
-            [tournament_day.day.strftime('%Y/%m/%d'), tournament_day.day]
-        end
-        if @tournament_days_array.size() > 0
-            @selected_tournament_day = params[:day] ? params[:day]  : @tournament_days_array[0][1]
-            @selected_tournament_day_matches = Match.eager_loading.where(day: @selected_tournament_day.in_time_zone.all_day, tournament_year: params[:id]).order(day: :desc)
+        @selected_tournament_day = params[:day]
+        @selected_tournament_day_matches = Match.eager_loading.where(day: @selected_tournament_day.in_time_zone.all_day, tournament_year: params[:id]).order(day: :desc)
+        respond_to do |format|
+            format.html { redirect_to @tournament }
+            format.js
         end
     end
-    
 end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,18 +1,19 @@
 class TournamentsController < ApplicationController
     def show
+        
         @years_array = TournamentYear.joins(:tournament).where(tournament: params[:id]).order(first_day: :desc).map do |tournament_year|
             [tournament_year.first_day.strftime('%Y'), tournament_year.id]
         end
-        puts "oooooooo"
-        puts params[:selected_tournament_year_id]
-        @selected_tournament_year_id = params[:selected_tournament_year_id] ? params[:selected_tournament_year_id] : @years_array[0][1]
-        # @tournament_year = TournamentYear.eager_loading.find(params[:id])
+        
+        @selected_tournament_year_id = @years_array[0][1]
+        
         @tournament_year = TournamentYear.eager_loading.find(@selected_tournament_year_id)
+
         @tournament_days_array = Match.joins(:tournament_year).select("day::date").where(tournament_year: params[:id]).group("day::date").order("day::date desc").map do |tournament_day|
             [tournament_day.day.strftime('%Y/%m/%d'), tournament_day.day]
         end
         if @tournament_days_array.size() > 0
-            @selected_tournament_day = params[:day] ? params[:day]  : @tournament_days_array[0][1]
+            @selected_tournament_day = @tournament_days_array[0][1]
             @selected_tournament_day_matches = Match.eager_loading.where(day: @selected_tournament_day.in_time_zone.all_day, tournament_year: params[:id]).order(day: :desc)
         end
     end
