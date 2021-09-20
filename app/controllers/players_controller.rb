1af1cff6be30a008396516c9154c9fa56522c5cf
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-20 12:28:42 +0900

    Remove shared/matches

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,7 +7,6 @@ class PlayersController < ApplicationController
     end
     @selected_tournament_year_and_player = TournamentYearAndPlayer.find(@tournament_years_array[0][1])
     @selected_tournament_year_player_matches = PlayerMatch.eager_loading.where(matches: {tournament_year: @selected_tournament_year_and_player.tournament_year.id}, player: @player.id)
-    
   end
 end
 
