4f2439ba04f2b76a2c965dc54f8fda791baf3063
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-20 11:57:57 +0900

    Remove comment out

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,12 +1,8 @@
 class PlayersController < ApplicationController
   def show
  
-    '''
-   
-    '''
     @player = Player.find(params[:id])
     @tournament_years_array = TournamentYearAndPlayer.eager_loading.where(player_id: @player.id).order('tournament_years.first_day DESC').map do |typ|
-      puts typ.id
       [typ.tournament_year.tournament.name + "(" + typ.tournament_year.first_day.strftime("%Y") + ")", typ.id]
     end
     @selected_tournament_year_and_player = TournamentYearAndPlayer.find(@tournament_years_array[0][1])
