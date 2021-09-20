9bc007c2bffffe62a70a1c5af603ad9e8dda5cdc
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-20 11:56:28 +0900

    remove format.html

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -4,7 +4,6 @@ class TournamentYearsController < ApplicationController
         @selected_tournament_day = params[:day]
         @selected_tournament_day_matches = Match.eager_loading.where(day: @selected_tournament_day.in_time_zone.all_day, tournament_year: params[:id]).order(day: :desc)
         respond_to do |format|
-            format.html { redirect_to @tournament }
             format.js
         end
     end
