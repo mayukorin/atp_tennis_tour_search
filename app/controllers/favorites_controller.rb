e64554cbb47175ed677ff52787f370341b1d26c0
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-21 14:34:35 +0900

    Add ajax to favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,19 @@
 class FavoritesController < ApplicationController
     def create
-        player = Player.find(params[:favorite_player_id])
-        current_user.make_favorite(player)
-        redirect_to player
+        @player = Player.find(params[:favorite_player_id])
+        current_user.make_favorite(@player)
+        respond_to do |format|
+            format.html { redirect_to @player }
+            format.js
+        end
     end
 
     def destroy
-        player = Favorite.find(params[:id]).player
-        current_user.destroy_favorite(player)
-        redirect_to player
+        @player = Favorite.find(params[:id]).player
+        current_user.destroy_favorite(@player)
+        respond_to do |format|
+            format.html { redirect_to @player }
+            format.js
+        end
     end
 end
