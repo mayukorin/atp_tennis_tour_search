8bd1cd575246ae2e6102457659fce2c9644e6e19
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-10-12 18:12:29 +0900

    Add and return to searches_controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,16 @@
 class SearchesController < ApplicationController
 
     def search
+        puts params[:name]
+        puts "okkkkkk"
         @tournament = Tournament.find_by(name: params[:name])
         if @tournament
-            redirect_to tournament_path(@tournament)
+            puts "wwww"
+            redirect_to tournament_path(@tournament) and return
         end
         @player = Player.find_by(name: params[:name])
         if @player
-            redirect_to player_path(@player)
+            redirect_to player_path(@player) and return
         end
         flash[:danger] = "検索結果が見つかりませんでした"
         redirect_to(params[:back_url])
