a53a26bb1792418305201947421886514adab76e
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-13 22:23:03 +0900

    Modify players show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,9 +5,11 @@ class PlayersController < ApplicationController
     @tournament_years_array = TournamentYearAndPlayer.eager_loading.where(player: @player.id).order('tournament_years.first_day DESC').map do |typ|
       [typ.tournament_year.tournament.name + "(" + typ.tournament_year.first_day.strftime("%Y") + ")", typ.id]
     end
-    @selected_tournament_year_and_player = TournamentYearAndPlayer.find(@tournament_years_array[0][1])
-    @selected_tournament_year_player_matches = PlayerMatch.eager_loading
-                                                .search_by_tournament_year_and_player(@selected_tournament_year_and_player.tournament_year.id, @player.id).in_order_of_latest
+    if @tournament_years_array.size > 0
+      @selected_tournament_year_and_player = TournamentYearAndPlayer.find(@tournament_years_array[0][1])
+      @selected_tournament_year_player_matches = PlayerMatch.eager_loading
+                                                  .search_by_tournament_year_and_player(@selected_tournament_year_and_player.tournament_year.id, @player.id).in_order_of_latest
+    end
   end
 end
 
