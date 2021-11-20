6bc085264534a6b6814b47742a516898e1d11447
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-20 13:44:35 +0900

    Add tournament year selection

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -1,8 +1,23 @@
 class TournamentYearsController < ApplicationController
 
     def show
+        '''
         @selected_tournament_day = params[:day]
         @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, params[:id]).order(day: :desc)
+        respond_to do |format|
+            format.js
+        end
+        '''
+        @selected_tournament_year_id = params[:selected_tournament_year_id]
+        
+        @tournament_year = TournamentYear.eager_loading.find(@selected_tournament_year_id)
+        @tournament_days_array = Match.joins(:tournament_year).select("day::date").where(tournament_year: params[:id]).group("day::date").order("day::date desc").map do |tournament_day|
+            [tournament_day.day.strftime('%Y/%m/%d'), tournament_day.day]
+        end
+        if @tournament_days_array.size() > 0
+            @selected_tournament_day = @tournament_days_array[0][1]
+            @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, params[:id]).order(day: :desc)
+        end
         respond_to do |format|
             format.js
         end
