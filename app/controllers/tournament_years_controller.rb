db976e0af639dfef415aff6c3bacb04371da1ad5
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-20 13:27:40 +0900

    Add tournament_years#day_show

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -7,4 +7,12 @@ class TournamentYearsController < ApplicationController
             format.js
         end
     end
+
+    def day_show
+        @selected_tournament_day = params[:day]
+        @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, params[:id]).order(day: :desc)
+        respond_to do |format|
+            format.js
+        end
+    end
 end
