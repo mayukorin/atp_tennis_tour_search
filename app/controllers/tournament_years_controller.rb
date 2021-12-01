56423cf977efeba673588ac137932659ca82c820
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-12-01 21:34:42 +0900

    rename array to collection

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -13,9 +13,9 @@ class TournamentYearsController < ApplicationController
         @tournament_year = TournamentYear.eager_loading.find(@selected_tournament_year_id)
 
 
-        @tournament_days_array = Match.select("to_char(day, 'YYYY/mm/dd') as day_str, day::date").joins(:tournament_year).where(tournament_year: params[:id]).group("to_char(day, 'YYYY/mm/dd'), day::date").order("day::date desc")
-        if @tournament_days_array.length > 0
-            @selected_tournament_day = @tournament_days_array.first[:day]
+        @tournament_days_collection = Match.select("to_char(day, 'YYYY/mm/dd') as day_str, day::date").joins(:tournament_year).where(tournament_year: params[:id]).group("to_char(day, 'YYYY/mm/dd'), day::date").order("day::date desc")
+        if @tournament_days_collection.length > 0
+            @selected_tournament_day = @tournament_days_collection.first[:day]
             @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, params[:id]).order(day: :desc)
         end
 
