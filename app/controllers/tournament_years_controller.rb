619dde3c837d54676a9118cbdb620fc7c91f812b
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-03-12 10:30:11 +0900

    fix:get tournament_year_id from not params[:id] but params[:selected_tournament_year_id] to display matches corresponding to the date

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -22,8 +22,7 @@ class TournamentYearsController < ApplicationController
     end
 
     def day_show
-        @selected_tournament_day = params[:day]
-        @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, params[:id]).order(day: :desc)
+        @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(params[:selected_tournament_day].in_time_zone.all_day, params[:selected_tournament_year_id]).order(day: :desc)
         respond_to do |format|
             format.js
         end
