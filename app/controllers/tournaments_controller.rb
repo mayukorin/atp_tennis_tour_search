6661a256db41322c88d968a3c9c8464cf32926bd
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-30 09:37:35 +0900

    refactor: remove join for unnecessary

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,24 +2,21 @@ class TournamentsController < ApplicationController
     def show
         @tournament = Tournament.find(params[:id])
        
-        @years_collection = TournamentYear.select("to_char(first_day, 'YYYY') as year, tournament_years.id").joins(:tournament).where(tournament: @tournament.id).order(first_day: :desc)
+        @years_collection = TournamentYear.select("to_char(first_day, 'YYYY') as year, tournament_years.id").where(tournament_id: @tournament.id).order(first_day: :desc)
         @selected_tournament_year_id = @years_collection.first[:id]
         
         @tournament_year = TournamentYear.eager_loading.find(@selected_tournament_year_id)
         
         # @tournament_days_collection = Match.select("to_char(day, 'YYYY/mm/dd') as day_str, day::date").joins(:tournament_year).where(tournament_year: @tournament_year.id).group("to_char(day, 'YYYY/mm/dd'), day::date").order("day::date desc")
         @tournament_days_collection = {}
-        Match.joins(:tournament_year).where(tournament_year: @tournament_year.id).pluck(:day).map do |date|
-            
-            # hash = {:day => date, :day_str => date.strftime("%Y/%m/%d") }
-            # Struct.new(*(hash.keys)).new(*(hash.values))
-            @tournament_days_collection[date.strftime("%Y/%m/%d")] = date
+        Match.where(tournament_year_id: @tournament_year.id).order(day: :desc).pluck(:day).map do |date|
+    
+            @tournament_days_collection[date.strftime("%Y/%m/%d").to_sym] = date
             
         end
-        @tournament_days_collection = @tournament_days_collection.sort.reverse
         
         if @tournament_days_collection.length > 0
-            @selected_tournament_day = @tournament_days_collection.first.second
+            @selected_tournament_day = @tournament_days_collection.values.first
             @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, @tournament_year.id).order(day: :desc)
             
         end
