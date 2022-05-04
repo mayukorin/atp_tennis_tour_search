0e8219a2dbdca19624610ee7cb7efbd34d435b7a
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-05-05 02:26:44 +0900

    refactor: refactor tournament show page

--- app/controllers/tournament_years_controller.rb
+++ app/controllers/tournament_years_controller.rb
@@ -1,28 +1,20 @@
 class TournamentYearsController < ApplicationController
 
-    def selected_tournament_year_show
+    def show
         @selected_tournament_year_id = params[:selected_tournament_year_id]
         @tournament_year = TournamentYear.eager_loading.find(@selected_tournament_year_id)
 
 
         @tournament_days_collection = {}
-        Match.joins(:tournament_year).where(tournament_year: @tournament_year.id).pluck(:day).map do |date|
-            @tournament_days_collection[date.strftime("%Y/%m/%d")] = date
+        Match.where(tournament_year: @tournament_year.id).order(day: :desc).pluck(:day).map do |date|
+            @tournament_days_collection[date.strftime("%Y/%m/%d").to_sym] = date
         end
         
-        @tournament_days_collection = @tournament_days_collection.sort.reverse
         if @tournament_days_collection.length > 0
-            @selected_tournament_day = @tournament_days_collection.first.second
+            @selected_tournament_day = @tournament_days_collection.values.first
             @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(@selected_tournament_day.in_time_zone.all_day, @selected_tournament_year_id).order(day: :desc)
         end
 
-        respond_to do |format|
-            format.js
-        end
-    end
-
-    def day_show
-        @selected_tournament_day_matches = Match.eager_loading.search_by_day_and_tournament_year(params[:selected_tournament_day].in_time_zone.all_day, params[:selected_tournament_year_id]).order(day: :desc)
         respond_to do |format|
             format.js
         end
